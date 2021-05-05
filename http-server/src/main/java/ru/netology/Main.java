aed434cc0bd73caee938581fc3cdb7376bc82dc6
stdd01 <stdd01@mail.ru>
2021-05-05 16:16:30 +0300

    add hw1

---
+++ http-server/src/main/java/ru/netology/Main.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import ru.netology.server.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    var server = new Server(64);
+    server.listen(9999);
+  }
+}
+
+
